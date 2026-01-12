61752a143616fe25108e415002547d9ef8b633fc
saiyedin786 <saiyedin786@gmail.com>
2026-01-12 15:10:27 +0530

    updated environment variable

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 # app/main.py
 
 from flask import Flask,render_template
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "default_key")
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    debug_mode = os.getenv("FLASK_DEBUG", "False") == "True"
+    port = int(os.getenv("FLASK_PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=debug_mode)
 
     
