2d19b826418575ca571ca75cf931396f02e077bd
saiyedin786 <saiyedin786@gmail.com>
2026-01-12 14:30:39 +0530

    index.html,style.css added and main.py updated

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 # app/main.py
 
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to the Dockerized Flask App!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
